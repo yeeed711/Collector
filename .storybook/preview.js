590ecaec463bb909426fcf889a184942d59ef37f
yeeed711 <rladpwl910@naver.com>
2022-08-27 16:36:09 +0900

    Feat: storybook 초기파일 셋팅

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,23 @@
+import { ThemeProvider } from 'styled-components';
+import { mainTheme } from '../src/theme';
+import { GlobalStyle } from '../src/index';
+
+export const decorators = [
+  (Stroy) => (
+    <>
+      <ThemeProvider theme={mainTheme} />
+      <GlobalStyle />
+      <Stroy />
+    </>
+  ),
+];
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
