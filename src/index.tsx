2461b07666560dfb24fb446bc07ad7e171011030
yeeed711 <rladpwl910@naver.com>
2022-08-27 16:35:12 +0900

    Feat: ThemProvider 설정 및 스타일 초기화

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import reset from 'styled-reset';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { mainTheme } from './theme';
+
+export const GlobalStyle = createGlobalStyle`
+  ${reset}
+  html {
+    font-size: 10px;
+  }
+  * {
+    box-sizing: border-box;
+  }
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+  button {
+    width: 100%;
+    background: none;
+    border: none;
+    padding: 0;
+    font: inherit;
+    cursor: pointer;
+  }
+  .ir {
+    position: absolute;
+    clip: rect(0,0,0,0);
+    width: 1px;
+    height: 1px;
+    margin: -1px;
+    overflow:hidden;
+  }
+  .hide {
+    display: none;
+  }
+`;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <ThemeProvider theme={mainTheme} />
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
-
-reportWebVitals();
