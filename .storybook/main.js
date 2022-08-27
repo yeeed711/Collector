f086fbe73a967af74547e43ae3595b0dda82b267
yeeed711 <rladpwl910@naver.com>
2022-08-27 19:10:24 +0900

    Chore: storybook-addon-pseudo-states 설치

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,7 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/preset-create-react-app',
+    'storybook-addon-pseudo-states',
   ],
   framework: '@storybook/react',
   core: {
