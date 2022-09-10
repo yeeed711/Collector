3eb40708e28f2cdfc33b2ecfb3152a2221e01f48
yeeed711 <rladpwl910@naver.com>
2022-09-10 23:06:22 +0900

    Delete: addon 제거

--- .storybook/main.tsx
+++ .storybook/main.tsx
@@ -6,7 +6,6 @@ module.exports = {
     '@storybook/addon-interactions',
     '@storybook/preset-create-react-app',
     'storybook-addon-pseudo-states',
-    'storybook-addon-styled-component-theme',
   ],
   framework: '@storybook/react',
   core: {
