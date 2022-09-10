f0be9c97debb9fd8b79d876eaa9fa4487fb224ba
yeeed711 <rladpwl910@naver.com>
2022-09-10 23:10:08 +0900

    Feat: 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Homepage from './components/pages/HomePage/Homepage';
+import JoinPage from './components/pages/JoinPage/JoinPage';
+import LoginPage from './components/pages/LoginPage/LoginPage';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/join' element={<JoinPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
