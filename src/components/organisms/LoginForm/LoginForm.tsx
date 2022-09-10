0a7ab1aaea2cdcc6889978b14d9498a866602aa6
yeeed711 <rladpwl910@naver.com>
2022-09-10 23:13:57 +0900

    Refactor: input atom 변경으로 인한 LoginForm Props 수정

--- src/components/organisms/LoginForm/LoginForm.tsx
+++ src/components/organisms/LoginForm/LoginForm.tsx
@@ -3,6 +3,7 @@ import Button from '../../atoms/Button/Button';
 import Input from '../../atoms/Input/Input';
 import Label from '../../atoms/Label/Label';
 import React, { useState } from 'react';
+import LinkBox from '../../molecules/LinkBox/LinkBox';
 
 interface Props {
   selected?: string | Fn;
@@ -13,7 +14,9 @@ type Fn = {
   (props: Props): string;
 };
 
-const FormContainer = styled.form<Props>``;
+const FormContainer = styled.form<Props>`
+  margin-bottom: 30px;
+`;
 
 const LoginStyle = styled.form`
   width: 550px;
@@ -22,6 +25,13 @@ const LoginStyle = styled.form`
   border: 1px solid #c4c4c4;
   border-radius: 10px;
   background-color: #ffffff;
+  input {
+    padding: 17px 0;
+    border-radius: 0;
+    &:focus {
+      border-bottom: 1px solid #21bf48;
+    }
+  }
   input + input {
     margin-top: 6px;
   }
@@ -42,9 +52,8 @@ const UserTypeContainer = styled.div<Props>`
     border-top-left-radius: 10px;
     margin-bottom: -20px;
     background-color: #f2f2f2;
-    z-index: -1;
+    /* z-index: -1; */
     border-bottom-style: none;
-    cursor: pointer;
     ${props =>
       props.selected === 'BUYER' &&
       css`
@@ -64,43 +73,42 @@ const LoginForm = () => {
   const [userType, setUserType] = useState('BUYER');
 
   return (
-    <FormContainer selected={userType}>
-      <UserTypeContainer>
-        <button onClick={() => setUserType('BUYER')}>구매회원 로그인</button>
-        <button onClick={() => setUserType('SELLER')}>판매회원 로그인</button>
-      </UserTypeContainer>
-      <LoginStyle>
-        <Label htmlFor='userId' children='아이디' className='ir' />
-        <Input
-          id='userId'
-          type='text'
-          placeholder='아이디'
-          border='none'
-          borderBottom='1px solid #C4C4C4'
-          padding='17px 0'
-        />
-        <Label htmlFor='password' children='비밀번호' className='ir' />
-        <Input
-          id='password'
-          type='text'
-          placeholder='비밀번호'
-          border='none'
-          borderBottom='1px solid #C4C4C4'
-          padding='17px 0'
-        />
-        <Button
-          type='submit'
-          label='로그인'
-          padding='19px 0'
-          borderRadius='5px'
-          fontSize='18px'
-          fontWeight='700'
-          lineHeight='22px'
-          color='#FFFFFF'
-          bgColor={props => props.theme.color.primary}
-        />
-      </LoginStyle>
-    </FormContainer>
+    <>
+      <FormContainer selected={userType}>
+        <UserTypeContainer>
+          <button onClick={() => setUserType('BUYER')}>구매회원 로그인</button>
+          <button onClick={() => setUserType('SELLER')}>판매회원 로그인</button>
+        </UserTypeContainer>
+        <LoginStyle>
+          <Label htmlFor='userId' children='아이디' className='ir' />
+          <Input
+            id='userId'
+            type='text'
+            placeholder='아이디'
+            className='square'
+          />
+          <Label htmlFor='password' children='비밀번호' className='ir' />
+          <Input
+            id='password'
+            type='password'
+            placeholder='비밀번호'
+            className='square'
+          />
+          <Button
+            type='submit'
+            label='로그인'
+            padding='19px 0'
+            borderRadius='5px'
+            fontSize='18px'
+            fontWeight='700'
+            lineHeight='22px'
+            color='#FFFFFF'
+            bgColor={props => props.theme.color.primary}
+          />
+        </LoginStyle>
+      </FormContainer>
+      <LinkBox />
+    </>
   );
 };
 export default LoginForm;
