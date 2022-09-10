c51727ee2b1ec7163de5912339fa49958c48c62c
yeeed711 <rladpwl910@naver.com>
2022-09-10 23:05:06 +0900

    Feat: logo props 추가

--- src/components/atoms/Logo/Logo.tsx
+++ src/components/atoms/Logo/Logo.tsx
@@ -4,10 +4,11 @@ import logo from '../../../assets/Logo-hodu.png';
 interface Props {
   alt: string;
   width: string;
+  height: string;
 }
 
-const Logo = ({ alt, width }: Props) => {
-  return <img src={logo} width={width} alt={alt} />;
+const Logo = ({ alt, width, height }: Props) => {
+  return <img src={logo} width={width} height={height} alt={alt} />;
 };
 
 export default Logo;
