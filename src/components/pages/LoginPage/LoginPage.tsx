839ef3d7fc499c5ec7802974aa4eab1ffc2de78f
yeeed711 <rladpwl910@naver.com>
2022-09-10 23:15:05 +0900

    Feat: LoginPage height Props 추가

--- src/components/pages/LoginPage/LoginPage.tsx
+++ src/components/pages/LoginPage/LoginPage.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import Logo from '../../atoms/Logo/Logo';
+import Header from '../../organisms/Header/Header';
 import LoginForm from '../../organisms/LoginForm/LoginForm';
 import LoginTemplate from '../../templates/LoginTemplate/LoginTemplate';
 
 const LoginPage = () => {
   return (
-    <LoginTemplate
-      header={<Logo width='238px' alt='호두샵 메인 로고' />}
-      content={<LoginForm />}
-      footer=''
-    />
+    <>
+      <Header />
+      <LoginTemplate
+        header={<Logo width='238px' height='74px' alt='호두샵 메인 로고' />}
+        content={<LoginForm />}
+      />
+    </>
   );
 };
 
