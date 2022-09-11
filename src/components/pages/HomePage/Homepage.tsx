946f518a244108528746c54490f4d55cd997d594
yeeed711 <rladpwl910@naver.com>
2022-09-11 19:38:58 +0900

    Feat: HomePage 컴포넌트에 Footer 추가

--- src/components/pages/HomePage/Homepage.tsx
+++ src/components/pages/HomePage/Homepage.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import Footer from '../../organisms/Footer/Footer';
 import Header from '../../organisms/Header/Header';
 import ProductList from '../../organisms/ProductList/ProductList';
 import MainTemplate from '../../templates/MainTemplate/MainTemplate';
 
 const Homepage = () => {
   return (
-    <MainTemplate header={<Header />} content={<ProductList />} footer='' />
+    <MainTemplate
+      header={<Header />}
+      content={<ProductList />}
+      footer={<Footer />}
+    />
   );
 };
 
